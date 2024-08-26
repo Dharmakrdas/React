9abade5fccb987890c5e3bb28dc1ff89a69c1eb7
Dharam Das <dharamdas111@gmail.com>
2024-08-26 18:57:52 +0530

    update res card

--- __test__/ResturentMenu.test.js
+++ __test__/ResturentMenu.test.js
@@ -48,11 +48,27 @@ it("Should be load Res menu component with offers", async () => {
   const resOfferCard = screen.getAllByTestId("resturent-offer");
 
   expect(resOfferCard.length).toBe(5);
-
-  const menuItems = screen.getAllByTestId("res-menu");
-
-  expect(menuItems.length).toBe(16);
 });
 
+it("Should be load with categories of menu list",async()=>{
+  global.fetch = () => {
+    return Promise.resolve({
+      json: () => {
+        return Promise.resolve(ResMenuMock);
+      },
+    });
+  };
+  await act(() => {
+    render(
+      <Provider store={appStore}>
+        <ResturentMenu />
+      </Provider>
+    );
+  });
+
+    const menuItems = screen.getAllByTestId("res-menu");
+
+    expect(menuItems.length).toBe(16);
+})
 
 
